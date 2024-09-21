aa2433dfa53541afc9d8914b64746b413335a28f
HackerTheFox <9a8m7t31@duck.com>
2024-09-21 14:10:28 -0500

    Maybe adding * to pythonanywhere.com domain will fix issue?

--- djangogirls/mysite/settings.py
+++ djangogirls/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-859asjd#sh5x0sb&f@c1j9354su&p3(gn^(#(v@nn)_*fbmrtr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "pythonanywhere.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "*.pythonanywhere.com"]
 
 
 # Application definition
